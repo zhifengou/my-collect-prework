77a17bb81c2583970ce57d411b135000b16b7dd0
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-29 20:47:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,34 @@
+def my_collect(arr)
+  if arr.size==0 
+    puts "it's an empty array"
+  elsif block_given?
+    i=0 
+    newArr=[]
+  while i<arr.size 
+    newArr.push(yield arr[i].upcase)
+    i+=1
+  end 
+  return newArr
+else 
+  "No block was given!"
+end 
+end
 
+def my_collect(arr)
+  if arr.size==0 
+    puts "it's an empty array"
+  elsif block_given?
+    i=0 
+    newArr=[]
+  while i<arr.size 
+    newArr.push(yield arr[i].split(" ").first)
+    i+=1
+  end 
+  return newArr
+else 
+  "No block was given!"
+end 
+end
+
+  
 
