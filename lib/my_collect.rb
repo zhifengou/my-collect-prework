ff9ef7212363adb05a4d84cd5d8f984b82fb4fdc
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-29 20:37:20 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(arr)
+  if arr.size==0 
+    puts "it's an empty array"
+  elsif block_given?
+    i=0 
+    newArr=[]
+  while i<arr.size 
+    newArr.push(yield arr[i].upcase)
+    i+=1
+  end 
+  return newArr
+else 
+  "No block was given!"
+end 
+end
 
+my_collect(arr) do |name|
+  name.split(" ").first 
+end
+
+
+  
 
