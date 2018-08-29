603a0e8585d6898d2ff31fdec635a12a99739fa5
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-29 20:42:13 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -14,10 +14,21 @@ else
 end 
 end
 
-my_collect(arr) do |name|
-  name.split(" ").first 
+def my_collect(arr)
+  if arr.size==0 
+    puts "it's an empty array"
+  elsif block_given?
+    i=0 
+    newArr=[]
+  while i<arr.size 
+    newArr.push(yield arr[i].upcase)
+    i+=1
+  end 
+  return newArr
+else 
+  "No block was given!"
+end 
 end
-
 
   
 
